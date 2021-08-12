253638407091c6d728eae1b7dc2da30e55edcca4
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-12 22:28:09 +0530

    variable name

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def musiconloop(file, stopper):
     mixer.music.load(file)
     mixer.music.play()
     while True:
-        a = input()
-        if a == stopper:
+        word = input()
+        if word == stopper:
             mixer.music.stop()
             break
 
