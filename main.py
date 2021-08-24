23145265efa34a0ec0d9a5900677b25ad930462b
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-24 09:19:41 +0530

    docs: add comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from pygame import mixer # pip install pygame
+from pygame import mixer
 from datetime import datetime
 from time import time
 
-
+# Function to play the music and stop it when the correct word is input by the user
 def musiconloop(file, stopper):
     mixer.init()
     mixer.music.load(file)
@@ -13,7 +13,7 @@ def musiconloop(file, stopper):
             mixer.music.stop()
             break
 
-
+# Logging the performed task in txt file
 def log_now(msg):
     with open('mylogs.txt', 'a') as f:
         f.write(f"{msg} {datetime.now()}\n")
@@ -28,18 +28,22 @@ if __name__ == '__main__':
     exersecs = 45*150
 
     while True:
+
+        # Initiate drinking water task
         if time()-init_water > watersecs:
             print("Water Drinking Time. Enter 'water' to stop the alarm.")
             musiconloop('paani.mp3', 'water')
             init_water = time()
             log_now("Drank water at")
 
+        # Initiate eyes exercise task
         if time()-init_eyes > eyessecs:
             print("Eyes Exercise Time. Enter 'eyes' to stop the alarm.")
             musiconloop('eyes.mp3', 'eyes')
             init_eyes = time()
             log_now("Eyes relaxed at")
 
+        #  Initiate physical exercise task
         if time()-init_exercise > exersecs:
             print("Physical Activity Time. Enter 'exer' to stop the alarm.")
             musiconloop('exercise.mp3', 'exer')
